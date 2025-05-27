25e009f141f224152b09e9fce907a6a94d74d8f8
Manikantan <manikantang11@gmail.com>
2025-05-26 19:05:51 -0500

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setupools import find_packages, setup
+
+setup(
+    name='ChatBot',
+    version='0.0.0',
+    author='Manikantan Ganesan',
+    author_email=manikantang11@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
