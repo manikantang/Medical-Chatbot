4d882d7db31d51a98bfc12cd84bbc115402c1413
Manikantan <manikantang11@gmail.com>
2025-05-26 20:37:34 -0500

    folder structure update

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setupools import find_packages, setup
+from setuptools import find_packages, setup
 
 setup(
     name='ChatBot',
     version='0.0.0',
     author='Manikantan Ganesan',
-    author_email=manikantang11@gmail.com',
+    author_email='manikantang11@gmail.com',
     packages=find_packages(),
     install_requires=[]
 )
